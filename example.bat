2b279ca832a30b11ef253a51035734548d1f3c0e
Adriru20 <124593111+adriru20@users.noreply.github.com>
2024-03-18 13:11:12 +0100

    Modificación de example.bat con configuración de CEIS

--- example.bat
+++ example.bat
@@ -3,8 +3,8 @@
 PowerShell.exe -ExecutionPolicy Bypass ^
 -File "%CD%\Sync-Contacts.ps1" ^
 -CredentialPath "%CD%\Credentials\credential.cred" ^
--Tenant "tenant.onmicrosoft.com" ^
--ContactFolderName "Global Address List" ^
+-Tenant "4e5d6375-3b91-4537-af13-53b7c61de53e" ^
+-ContactFolderName "TestGAL" ^
 -LogPath "%CD%\Logs" ^
--AzureADGroup "SG_GALSyncUsers"
+-AzureADGroup "GAL_Sync"
 pause
