32e6b2247870059f653c0a9c340fc9c525ec6a36
Adriru20 <124593111+adriru20@users.noreply.github.com>
2024-04-05 13:06:08 +0200

    Resubir cambios en el script

--- example.bat
+++ example.bat
@@ -4,7 +4,8 @@ PowerShell.exe -ExecutionPolicy Bypass ^
 -File "%CD%\Sync-Contacts.ps1" ^
 -CredentialPath "%CD%\Credentials\credential.cred" ^
 -Tenant "4e5d6375-3b91-4537-af13-53b7c61de53e" ^
--ContactFolderName "TestGAL" ^
+-ContactFolderName "CEIS" ^
 -LogPath "%CD%\Logs" ^
 -AzureADGroup "GAL_Sync"
+
 pause
