3d310636c05dd3110d5f6b6a714533c6942b50b6
Adriru20 <124593111+adriru20@users.noreply.github.com>
2024-04-05 13:07:37 +0200

    Add files via upload

---
+++ GlobalAddressList/Module/Public/Sync/func_Update-FolderContact.ps1
@@ -0,0 +1,29 @@
+function Update-FolderContact {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory)][object]$ContactFolder,
+        [parameter(Mandatory)][object]$Contact
+    )
+    try {
+        $updateContactBody = @{
+            givenName      = $Contact.givenName
+            surname        = $Contact.surname
+            jobTitle       = $Contact.jobTitle
+            department     = $Contact.department
+            emailAddresses = $Contact.emailAddresses
+        }
+        # add these based on pressence
+        # if ($Contact.homePhones) { $updateContactBody.homePhones = $NewContact.homePhones }
+        if ($Contact.businessPhones) { $updateContactBody.businessPhones = $Contact.businessPhones }
+        if ($UseGraphSDK) {
+            Update-MgUserContactFolderContact -UserId $ContactFolder.mailBox -ContactFolderId $ContactFolder.id -ContactId $Contact.id -BodyParameter $updateContactBody
+        } else {
+            New-GraphRequest -Method Patch -Endpoint "/users/$($ContactFolder.mailBox)/contactFolders/$($ContactFolder.id)/contacts/$($Contact.id)" -Body $updateContactBody | Out-Null
+        }
+        Write-VerboseEvent "Updated contact $($Contact.displayName)"
+        return $true
+    }
+    catch {
+        throw (Format-ErrorCode $_).ErrorMessage
+    }
+}
