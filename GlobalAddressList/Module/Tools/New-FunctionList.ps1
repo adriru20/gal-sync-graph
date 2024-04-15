e74657bf35e7b52529ac5bdd7fe31f172e3f6fef
Adriru20 <124593111+adriru20@users.noreply.github.com>
2024-04-15 08:20:40 +0200

    Borrado completo

--- GlobalAddressList/Module/Tools/New-FunctionList.ps1
+++
@@ -1,7 +0,0 @@
-function New-FunctionList {
-    $funcList = @()
-    Get-ChildItem ($pwd.Path + "/Public") -Recurse -Filter "func_*.ps1" | ForEach-Object {
-        $funcList += ($_.BaseName -Split "_")[1]
-    }
-    return $funcList | Sort-Object
-}
--- Module/Public/func_Write-VerboseEvent.ps1
+++
@@ -1,5 +0,0 @@
-function Write-VerboseEvent ($Message) {
-    $timeStamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-    $verboseOutput = $timeStamp, "Verbose", $Message -join " | "
-    Write-Verbose $verboseOutput
-}
